d9b182d79d6bf0d9cbed6f0cdaec22befc35b3bc
ajdominic <ajdominiciii@outlook.com>
2022-04-23 17:20:34 -0600

    added old code for the creation of the spin lattice

---
+++ old_code.py
@@ -0,0 +1,10 @@
+dim = 3
+spins = ['1', '0'] 
+possible_configs = ["".join(item) for item in it.product(spins, repeat=9)]
+spin_tensor = np.zeros([dim, dim, configs])
+for k in range(configs):
+    config_temp = possible_configs[k] 
+    temp = [char for char in config_temp]
+    array_temp = np.array(temp, float)
+    reshaped = np.reshape(array_temp, (3, 3))
+    spin_tensor[:, :, k] = np.where(reshaped == 0, -1, reshaped)
